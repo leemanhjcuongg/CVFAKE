b25bd75e8251180b08d284544ba7c9881ba5d845
Lê Mạnh Cường <cuong.dz.1806@gmail.com>
2021-09-17 23:00:58 +0700

    part n
    add discord
    fix some bugs

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ close.addEventListener("click", () => {
     textbox.classList.remove('active');
     bg_img.classList.remove('active');
 });
+
+// var $cursor = $('.cursor');
+
+// function moveCursor(e) {
+//   $cursor.addClass('is-moving');
+  
+// 	TweenLite.to($cursor, 0.23, {
+//     left: e.pageX,
+//     top: e.pageY,
+//     ease: Power4.easOut
+//   });
+  
+//   clearTimeout(timer);
+
+//    var timer = setTimeout(function() {
+//        $cursor.removeClass('is-moving');
+//    }, 300);
+// }
+
+// $(window).on('mousemove', moveCursor);
